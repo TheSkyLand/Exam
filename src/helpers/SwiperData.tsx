b1684755ae2498bb19143dd26bb68baa9d032c6d
George <77775699+TheSkyLand@users.noreply.github.com>
2025-11-23 17:17:28 +0300

    Update SwiperData.tsx

--- src/helpers/SwiperData.tsx
+++ src/helpers/SwiperData.tsx
@@ -1,34 +1,200 @@
 export const SwiperData = [
     {
-        date: "1980",
-        info: "13 сентября — частное солнечное затмение, видимое в Южной Африке и части Антарктиды"
+        id: 1,
+        data: [
+            {
+                date: "1980",
+                info: ""
+            },
+            {
+                date: "1981",
+                info: "agag"
+            },
+            {
+                date: "1982",
+                info: "asgasg"
+            },
+            {
+                date: "1983",
+                info: "asgasg"
+            },
+            {
+                date: "1984",
+                info: "asgasg"
+            },
+            {
+                date: "1985",
+                info: "asgasg"
+            },
+            {
+                date: "1986",
+                info: "asgasg"
+            }
+        ]
     },
-    {
-        date: "1981",
-        info: "agag"
+        {
+        id: 2,
+        data: [
+            {
+                date: "1980",
+                info: ""
+            },
+            {
+                date: "1981",
+                info: "agag"
+            },
+            {
+                date: "1982",
+                info: "asgasg"
+            },
+            {
+                date: "1983",
+                info: "asgasg"
+            },
+            {
+                date: "1984",
+                info: "asgasg"
+            },
+            {
+                date: "1985",
+                info: "asgasg"
+            },
+            {
+                date: "1986",
+                info: "asgasg"
+            }
+        ]
     },
-    {
-        date: "1982",
-        info: "asgasg"
+        {
+        id: 3,
+        data: [
+            {
+                date: "1980",
+                info: ""
+            },
+            {
+                date: "1981",
+                info: "agag"
+            },
+            {
+                date: "1982",
+                info: "asgasg"
+            },
+            {
+                date: "1983",
+                info: "asgasg"
+            },
+            {
+                date: "1984",
+                info: "asgasg"
+            },
+            {
+                date: "1985",
+                info: "asgasg"
+            },
+            {
+                date: "1986",
+                info: "asgasg"
+            }
+        ]
     },
-    {
-        date: "1983",
-        info: "asgasg"
+        {
+        id: 4,
+        data: [
+            {
+                date: "1980",
+                info: ""
+            },
+            {
+                date: "1981",
+                info: "agag"
+            },
+            {
+                date: "1982",
+                info: "asgasg"
+            },
+            {
+                date: "1983",
+                info: "asgasg"
+            },
+            {
+                date: "1984",
+                info: "asgasg"
+            },
+            {
+                date: "1985",
+                info: "asgasg"
+            },
+            {
+                date: "1986",
+                info: "asgasg"
+            }
+        ]
     },
-    {
-        date: "1984",
-        info: "asgasg"
+        {
+        id: 5,
+        data: [
+            {
+                date: "1980",
+                info: ""
+            },
+            {
+                date: "1981",
+                info: "agag"
+            },
+            {
+                date: "1982",
+                info: "asgasg"
+            },
+            {
+                date: "1983",
+                info: "asgasg"
+            },
+            {
+                date: "1984",
+                info: "asgasg"
+            },
+            {
+                date: "1985",
+                info: "asgasg"
+            },
+            {
+                date: "1986",
+                info: "asgasg"
+            }
+        ]
     },
-    {
-        date: "1985",
-        info: "asgasg"
-    },
-    {
-        date: "1986",
-        info: "asgasg"
-    },
-    {
-        date: "1983",
-        info: "agsgassga"
+        {
+        id: 6,
+        data: [
+            {
+                date: "1980",
+                info: ""
+            },
+            {
+                date: "1981",
+                info: "agag"
+            },
+            {
+                date: "1982",
+                info: "asgasg"
+            },
+            {
+                date: "1983",
+                info: "asgasg"
+            },
+            {
+                date: "1984",
+                info: "asgasg"
+            },
+            {
+                date: "1985",
+                info: "asgasg"
+            },
+            {
+                date: "1986",
+                info: "asgasg"
+            }
+        ]
     }
 ]
